6c21dd1277953992a81e64d6a666a274a9b78ada
bala-amypo <bala@amypo.in>
2025-12-17 05:25:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,11 +1,9 @@
 package com.example.demo.entity;
+import jarkata.persistence.*;
 
-
-import jarkata.persistence.Id;
 @Entity
-
 public class Student{
-    @Id
+    @Id //primary key 
     @GenerateValue(strategy+GenerationType.IDENTITY)
     private Integer id;
     private String name;
@@ -29,7 +27,7 @@ public class Student{
     public void setEmail(String email){
         this.email=email;
     }
-    public Student(int id,String name,String email){
+    public Student(Integer id,String name,String email){
         this.id=id;
         this.name=name;
         this.email=email;
